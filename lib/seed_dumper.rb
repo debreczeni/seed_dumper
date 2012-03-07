aa92b1050cca3ac69d725ee8695420b61a49f9dc
David Debreczeni <david@octarine-labs.com>
2012-03-07 16:17:32 +0100

    version bump

--- lib/seed_dumper.rb
+++ lib/seed_dumper.rb
@@ -2,7 +2,7 @@ require 'rake' unless defined?(Rake)
 require 'rails' unless defined?(Rails)
 
 module SeedDumper
-  VERSION = '0.1.3'
+  VERSION = '0.1.4'
 
   autoload :Fetcher, 'seed_dumper/fetcher'
   autoload :Writer, 'seed_dumper/writer'
